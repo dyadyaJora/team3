4a8d9af9691fcf0efb879505c7ca270790d495c8
Vasiliy <vvasiliy29@gmail.com>
2016-08-13 15:48:42 +0300

    delete .idea

--- static/src/app/pepo.js
+++ static/src/app/pepo.js
@@ -1,18 +1,18 @@
 window.pepo = angular.module('pepo', ['ngRoute'])
-  .config(function($routeProvider) {
+    .config(function ($routeProvider) {
 
-    $routeProvider
-      .when('/', {
-        templateUrl: './build/templates/login.html',
-        controller: 'loginCtrl'
-      })
-      .when('/choose-login', {
-        templateUrl: './build/templates/choose-login.html',
-        controller: 'chooseLoginCtrl'
-      })
-        .when('/my-profile', {
-            templateUrl: './build/templates/my-profile.html',
-            controller: 'myProfile'
-        })
-      .otherwise({redirectTo: '/'});
-  });
+        $routeProvider
+            .when('/', {
+                templateUrl: './build/templates/login.html',
+                controller: 'loginCtrl'
+            })
+            .when('/choose-login', {
+                templateUrl: './build/templates/choose-login.html',
+                controller: 'chooseLoginCtrl'
+            })
+            .when('/my-profile', {
+                templateUrl: './build/templates/my-profile.html',
+                controller: 'myProfileCtrl'
+            })
+            .otherwise({redirectTo: '/'});
+    });
