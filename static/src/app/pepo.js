01301e2ad2ca01c9ff0d34d2eba6abd6ba6dc75c
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-08-13 16:16:28 +0300

    :rocket: Feed page.

--- static/src/app/pepo.js
+++ static/src/app/pepo.js
@@ -1,18 +1,22 @@
 window.pepo = angular.module('pepo', ['ngRoute'])
     .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: './build/templates/login.html',
+        controller: 'loginCtrl'
+      })
+      .when('/choose-login', {
+        templateUrl: './build/templates/choose-login.html',
+        controller: 'chooseLoginCtrl'
+      })
+      .when('/my-profile', {
+          templateUrl: './build/templates/my-profile.html',
+          controller: 'myProfile'
+      })
+      .when('/feed', {
+          templateUrl: './build/templates/feed.html',
+          controller: 'feedCtrl'
+      })
+      .otherwise({redirectTo: '/'});
+  });
 
-        $routeProvider
-            .when('/', {
-                templateUrl: './build/templates/login.html',
-                controller: 'loginCtrl'
-            })
-            .when('/choose-login', {
-                templateUrl: './build/templates/choose-login.html',
-                controller: 'chooseLoginCtrl'
-            })
-            .when('/my-profile', {
-                templateUrl: './build/templates/my-profile.html',
-                controller: 'myProfileCtrl'
-            })
-            .otherwise({redirectTo: '/'});
-    });
---
+++ static/src/app/services/userApi.js
@@ -0,0 +1,13 @@
+// pepo.service('userApi', function($resource) {
+//   $resource('/api/:content/:prefix', {},
+//     getUsers: {
+//       method: 'GET',
+//       isArray: false,
+//       params: {
+//         content: ''
+//       }
+//     }
+
+//   );
+// });
+
