f662508d80b1f57cf1d10099f29c53552ed997a7
rustem1022 <alex161994@mail.ru>
2016-08-13 23:18:00 +0300

    find_header

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('jsConcat', function () {
 });
 
 gulp.task('templates', function () {
-    gulp.src(path.src.templates)
+    gulp.src(path.src.templates) 
         .pipe(gulp.dest(path.dist.templates));
 });
 
