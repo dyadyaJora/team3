19666f40e8ca698a35d3faf397dd04fdde83b67c
rustem1022 <alex161994@mail.ru>
2016-08-15 13:43:55 +0300

    new_menu

--- static/src/app/controllers/chooseLoginCtrl.js
+++ static/src/app/controllers/chooseLoginCtrl.js
@@ -1,21 +1,3 @@
-pepo.controller('chooseLoginCtrl', function($q, $scope, $auth, userApi){
+pepo.controller('chooseLoginCtrl', function($scope){
   console.log('chooseLoginCtrl');
-
-  $scope.authenticate = function(provider) {
-      console.log('auth');
-      console.log($auth.authenticate(provider));
-  };
-
-  $scope.isAuthenticated = function() {
-    return $auth.isAuthenticated();
-  };
-
-  $scope.logout = function() {
-    $auth.logout();
-  }
-
-  $q.when(userApi.getUser().$promise).then(function(data){
-    console.log('get user');
-    console.log(data);
-  });
 });
