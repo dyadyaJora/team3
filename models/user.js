9389b8407f411da494b81b2ac66e06d09daeaba2
Andrey Popovich <and.popovich@gmail.com>
2016-08-15 14:41:33 +0300

    Add user validations

--- models/user.js
+++ models/user.js
@@ -2,13 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  username: String,
-  name: String,
+  username: { type: String, required: true },
+  name: { type: String, required: true },
   fbId: Number,
   vkId: Number,
-  token: String
+  token: { type: String, required: true }
 }, {
   timestamps: true
 });
+
+userSchema.path('username').validate(function(value, done) {
+  this.model('User').count({ username: value }, function(err, count) {
+    if (err) { return done(err); }
+
+    done(!count);
+  });
+}, 'такой логин уже зарегистрирован');
 
 mongoose.model('User', userSchema);
