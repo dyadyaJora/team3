3f20bfe79772979090a4d44a8dd29c7079c52861
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-08-18 19:51:44 +0300

    :wrench: Mobile layout for feed page.

--- static/src/app/services/mockTweets.js
+++ static/src/app/services/mockTweets.js
@@ -53,6 +53,74 @@ pepo.constant('MOCKTWEETS',
   {
     "id": 2,
     "parentId": 1,
+    "text": "Eat my shorts doesn't mean eat my shorts literally.",
+    "imageUrl": "http://url/to/image.jpg",
+    "location": [-73.856077, 40.848447],
+    "owner": {
+      "id": 1,
+      "username": "bartman",
+      "name": "Bart Simpson",
+      "avatarUrl": "http://url/to/avatar.jpg",
+      "thumbUrl": "build/img/bart.jpg"
+    },
+    "children": [
+      {
+        "id": 3,
+        "parentId": 2,
+        "text": "Lorem ipsum dolor...",
+        "imageUrl": "http://url/to/image.jpg",
+        "location": [-73.856077, 40.848447],
+        "owner": {
+          "id": 1,
+          "username": "username",
+          "name": "John Doe",
+          "avatarUrl": "http://url/to/avatar.jpg",
+          "thumbUrl": "http://url/to/thumb.jpg"
+        },
+        "createdAt": "2016-08-13T00:00:00Z",
+        "updatedAt": "2016-08-13T00:00:00Z"
+      }
+    ],
+    "createdAt": "12:22 14.02.16",
+    "updatedAt": "2016-08-13T00:00:00Z"
+  },
+  {
+    "id": 2,
+    "parentId": 1,
+    "text": "Poor Krusty. He's like black velvet painting come to life.",
+    "imageUrl": "http://url/to/image.jpg",
+    "location": [-73.856077, 40.848447],
+    "owner": {
+      "id": 1,
+      "username": "username",
+      "name": "Lisa Simpson",
+      "avatarUrl": "http://url/to/avatar.jpg",
+      "thumbUrl": "build/img/lisa.jpg"
+    },
+    "children": [
+      {
+        "id": 3,
+        "parentId": 2,
+        "text": "Lorem ipsum dolor...",
+        "imageUrl": "http://url/to/image.jpg",
+        "location": [-73.856077, 40.848447],
+        "owner": {
+          "id": 1,
+          "username": "username",
+          "name": "John Doe",
+          "avatarUrl": "http://url/to/avatar.jpg",
+          "thumbUrl": "http://url/to/thumb.jpg"
+        },
+        "createdAt": "2016-08-13T00:00:00Z",
+        "updatedAt": "2016-08-13T00:00:00Z"
+      }
+    ],
+    "createdAt": "12:22 14.02.16",
+    "updatedAt": "2016-08-13T00:00:00Z"
+  },
+  {
+    "id": 2,
+    "parentId": 1,
     "text": "Just because Maggie can't talk doesn't mean she's dumb. Einstein didn't speak until he was three.",
     "imageUrl": "http://url/to/image.jpg",
     "location": [-73.856077, 40.848447],
@@ -62,6 +130,90 @@ pepo.constant('MOCKTWEETS',
       "name": "Lisa Simpson",
       "avatarUrl": "http://url/to/avatar.jpg",
       "thumbUrl": "build/img/lisa.jpg"
+    },
+    "children": [
+      {
+        "id": 3,
+        "parentId": 2,
+        "text": "Lorem ipsum dolor...",
+        "imageUrl": "http://url/to/image.jpg",
+        "location": [-73.856077, 40.848447],
+        "owner": {
+          "id": 1,
+          "username": "username",
+          "name": "John Doe",
+          "avatarUrl": "http://url/to/avatar.jpg",
+          "thumbUrl": "http://url/to/thumb.jpg"
+        },
+        "createdAt": "2016-08-13T00:00:00Z",
+        "updatedAt": "2016-08-13T00:00:00Z"
+      }
+    ],
+    "createdAt": "12:22 14.02.16",
+    "updatedAt": "2016-08-13T00:00:00Z"
+  },
+  {
+    "id": 2,
+    "parentId": 1,
+    "text": "Poor Krusty. He's like black velvet painting come to life.",
+    "imageUrl": "http://url/to/image.jpg",
+    "location": [-73.856077, 40.848447],
+    "owner": {
+      "id": 1,
+      "username": "lisa",
+      "name": "Lisa Simpson",
+      "avatarUrl": "http://url/to/avatar.jpg",
+      "thumbUrl": "build/img/lisa.jpg"
+    },
+    "childrens": [
+      {
+        "id": 3,
+        "parentId": 2,
+        "text": "Ay, Karamba!",
+        "imageUrl": "http://url/to/image.jpg",
+        "location": [-73.856077, 40.848447],
+        "owner": {
+          "id": 1,
+          "username": "bartolomew",
+          "name": "Bart Simpson",
+          "avatarUrl": "http://url/to/avatar.jpg",
+          "thumbUrl": "build/img/bart.jpg"
+        },
+        "createdAt": "2016-08-13T00:00:00Z",
+        "updatedAt": "2016-08-13T00:00:00Z"
+      },
+      {
+        "id": 3,
+        "parentId": 2,
+        "text": "Eat my shorts!",
+        "imageUrl": "http://url/to/image.jpg",
+        "location": [-73.856077, 40.848447],
+        "owner": {
+          "id": 1,
+          "username": "bartolomew",
+          "name": "Bart Simpson",
+          "avatarUrl": "http://url/to/avatar.jpg",
+          "thumbUrl": "build/img/bart.jpg"
+        },
+        "createdAt": "2016-08-13T00:00:00Z",
+        "updatedAt": "2016-08-13T00:00:00Z"
+      }
+    ],
+    "createdAt": "12:22 14.02.16",
+    "updatedAt": "2016-08-13T00:00:00Z"
+  },
+  {
+    "id": 2,
+    "parentId": 1,
+    "text": "Eat my shorts doesn't mean eat my shorts literally.",
+    "imageUrl": "http://url/to/image.jpg",
+    "location": [-73.856077, 40.848447],
+    "owner": {
+      "id": 1,
+      "username": "bartman",
+      "name": "Bart Simpson",
+      "avatarUrl": "http://url/to/avatar.jpg",
+      "thumbUrl": "build/img/bart.jpg"
     },
     "children": [
       {
