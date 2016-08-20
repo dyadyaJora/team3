9dab4d1fc6479115275760a88440cf7d55374d34
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-08-20 17:46:32 +0300

    :wrench: Live add/delete peps.

--- static/src/app/controllers/feedCtrl.js
+++ static/src/app/controllers/feedCtrl.js
@@ -1,23 +1,29 @@
-pepo.controller('feedCtrl', function($q, $location, $auth, $scope, pepsApi, MOCKTWEETS) {
-  console.log('feedCtrl');
+pepo.controller('feedCtrl', function($q, $location, $auth, $scope, userApi, pepsApi, MOCKTWEETS) {
   $scope.newPepText = '';
   pepsApi.getPeps().$promise.then(function(data){
     $scope.tweets = data;
   });
+  userApi.getUser().$promise.then(function(data) {
+    $scope.currentUser = data;
+  });
 
   $scope.sendPep = function() {
     newPep = {
+      owner: {
+        name: $scope.currentUser.name,
+        username: $scope.currentUser.username
+      },
       text: $scope.newPepText
     }
     pepsApi.sendPep(newPep).$promise.then(function(data){
-      $scope.tweets.push(newPep);
+      newPep._id = data._id
+      $scope.tweets.unshift(newPep);
     })
     .catch(function(err) {
       console.log(err);
     })
     $scope.varAnswer = false;
   }
-
 
   $scope.tweets = MOCKTWEETS;
 
@@ -35,7 +41,6 @@ pepo.controller('feedCtrl', function($q, $location, $auth, $scope, pepsApi, MOCK
   $scope.openModalAnswer = function(id) {
     $scope.varAnswer = true;
   	$scope.pep = $scope.tweets[id];
-
   }
 
   $scope.openModalDel = function(index, id) {
@@ -43,7 +48,6 @@ pepo.controller('feedCtrl', function($q, $location, $auth, $scope, pepsApi, MOCK
   	$scope.pep = $scope.tweets[index];
     $scope.delIndex = index;
     $scope.delId = id;
-    console.log(id);
   }
 
   $scope.closeModalAnswer = function($event){
@@ -51,10 +55,8 @@ pepo.controller('feedCtrl', function($q, $location, $auth, $scope, pepsApi, MOCK
 	if(click.hasClass("modal")){
 		$scope.varAnswer=false;
 		$scope.varDel=false;
-	}
+	  }
   }
-
-
 
   $scope.deletePep = function(){
     pepsApi.deletePep({id: $scope.delId}).$promise.then(function(data){
@@ -62,7 +64,7 @@ pepo.controller('feedCtrl', function($q, $location, $auth, $scope, pepsApi, MOCK
       $scope.varDel=false;
   }).catch(function(eror){
     $scope.varDel=false;
-  });   
+  });
 
   }
 
