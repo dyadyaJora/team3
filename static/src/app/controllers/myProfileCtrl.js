3c035a8fdf67fb18ac9c1db847e97b86fcb6b236
Vasiliy <Sh29@gmail.com>
2016-08-20 13:14:01 +0300

    add user profile

--- static/src/app/controllers/myProfileCtrl.js
+++ static/src/app/controllers/myProfileCtrl.js
@@ -1,3 +1,13 @@
-pepo.controller('myProfileCtrl', function($scope){
-    console.log('My profile');
+pepo.controller('myProfileCtrl', function($location, $auth, $scope, MOCKTWEETS){
+    console.log('myProfileCtrl');
+    $scope.tweets = MOCKTWEETS;
+
+    $scope.logout = function() {
+        $auth.logout();
+        $location.path('/');
+    };
+
+    $scope.goToPep = function(pepId) {
+        $location.path('/pep' + pepId);
+    }
 });
