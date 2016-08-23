e572fc377613d86d5fbb6255e5a10d6d3bf71dde
dyadyaJora <ja_massa97@mail.ru>
2016-08-24 02:09:32 +0300

    try to load real users

--- static/src/app/controllers/usersCtrl.js
+++ static/src/app/controllers/usersCtrl.js
@@ -1,6 +1,12 @@
-pepo.controller('usersCtrl', function($location, $scope, MOCKUSERS) {
-  $scope.users = MOCKUSERS;
+pepo.controller('usersCtrl', function($location, $scope, usersApi, userApi, MOCKUSERS) {
+  //$scope.users = MOCKUSERS;
   $scope.searchValue = '';
+  console.log(userApi);
+
+  usersApi.getUsers().$promise.then(function(data) {
+    $scope.users = data;
+    console.log(data);
+  }); 
 
   $scope.goToUser = function(username) {
     $location.path('/@' + username);
