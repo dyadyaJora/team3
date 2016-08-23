bb444b696cc14f1bca46bdd68df111c47d82707f
dyadyaJora <ja_massa97@mail.ru>
2016-08-24 02:32:16 +0300

    delete log, create catch

--- static/src/app/controllers/usersCtrl.js
+++ static/src/app/controllers/usersCtrl.js
@@ -1,11 +1,11 @@
 pepo.controller('usersCtrl', function($location, $scope, usersApi, userApi, MOCKUSERS) {
   //$scope.users = MOCKUSERS;
   $scope.searchValue = '';
-  console.log(userApi);
 
   usersApi.getUsers().$promise.then(function(data) {
     $scope.users = data;
-    console.log(data);
+  }).catch(function(eror){
+  	console.log(eror);
   }); 
 
   $scope.goToUser = function(username) {
