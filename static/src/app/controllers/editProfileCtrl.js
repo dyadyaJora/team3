2337451daf0b0069124d6b33b11add93bb14cf6c
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-08-25 17:00:51 +0300

    :rocket: Load/seve username and avatar on edit-profile page.

--- static/src/app/controllers/editProfileCtrl.js
+++ static/src/app/controllers/editProfileCtrl.js
@@ -1,23 +1,27 @@
-pepo.controller('editProfileCtrl', function ($location, $scope, MOCKUSERS, multipartForm) {
-    console.log('Edit profile');
-    $scope.user = MOCKUSERS[1];
+pepo.controller('editProfileCtrl', function ($location, $scope, userApi,  MOCKUSERS, multipartForm) {
+
+    userApi.getUser().$promise.then(function(data) {
+      $scope.user = data;
+    });
+
     $scope.varOpenEditPhoto = false;
     $scope.userEdit = {};
+
     $scope.updateUser = function () {
-      console.log($scope.userEdit);
+      $scope.userEdit.username = $scope.user.username;
+      $scope.userEdit.description = $scope.user.description;
       var uploadUrl = '/api/user';
       multipartForm.patch(uploadUrl, $scope.userEdit);
-
-
-        // $location.path('/feed');
     };
 
     $scope.editCancel = function () {
         $location.path('/feed');
     };
+
     $scope.openModalEditPhoto = function () {
         $scope.varOpenEditPhoto = true;
     };
+
     $scope.closeModalEditPhoto = function () {
         $scope.varOpenEditPhoto = false;
     }
