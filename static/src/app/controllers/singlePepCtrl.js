a47ca4c19152c7f36c55f0f895c5dc343fc4281b
dyadyaJora <ja_massa97@mail.ru>
2016-08-30 23:13:57 +0300

    follow button at owner of pep

--- static/src/app/controllers/singlePepCtrl.js
+++ static/src/app/controllers/singlePepCtrl.js
@@ -1,10 +1,16 @@
 pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, MOCKTWEETS) {
 
-  currentPepId = $location.path().slice(4);
 
+  $scope.$on('currentUserLoaded', function() {
+    $scope.curUser = $scope.currentUser;
+    console.log($scope.currentUser);
+  });
+
+  currentPepId = $location.path().slice(4);
   pepsApi.getSinglePep({id: currentPepId}).$promise.then(function(data) {
     $scope.currentTweet = data;
     $scope.parent = $scope.currentTweet.parent;
+    checkFollow();
   });
 
   $scope.isOwner = function(user, tweetOwner) {
@@ -14,4 +20,14 @@ pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, M
   $scope.goToUser = function(username) {
     $location.path('/@' + username);
   }
+
+  function checkFollow() {
+     $scope.$on('currentUserLoaded', function() {
+       $scope.currentUser.following.some(function(followingUser) {
+         if (followingUser === $scope.currentTweet.owner._id) {
+          $scope.followed = true;
+         }
+       });
+     });
+   }
 });
