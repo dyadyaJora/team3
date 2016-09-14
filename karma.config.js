f36c5f75b3988113e06cbf39e8ec763c8bf39c12
dyadyaJora <ja_massa97@mail.ru>
2016-09-14 17:54:42 +0300

    devDependencies for karma

--- karma.config.js
+++ karma.config.js
@@ -65,8 +65,9 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'PhantomJS'],
 
+    //browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
