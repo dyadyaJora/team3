ab4a52a7d7b71e663603208b02458d012bcf367e
dyadyaJora <ja_massa97@mail.ru>
2016-09-19 01:28:57 +0300

    formating for not-found pep and add controller for different 404 pep or user

--- static/src/app/controllers/notFoundCtrl.js
+++ static/src/app/controllers/notFoundCtrl.js
@@ -1,3 +1,14 @@
 pepo.controller('notFoundCtrl', function($location, $scope, pepsApi, userApi, usersApi, MOCKTWEETS) {
-
+	var type = $location.path().slice(11);
+	console.log(type);
+	switch(type){
+		case "pep":
+			$scope.errorText = "К сожалению данный пеп не найден =(";
+			break;
+		case "user":
+			$scope.errorText = "Искомый пользователь в системе не обнаружен =(";
+			break;
+		default:
+			$scope.errorText = "";
+	}
 });
