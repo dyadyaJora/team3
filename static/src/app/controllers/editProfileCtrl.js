e28f86c3ab8e9bac112062cbe02e4235e3b003c3
dyadyaJora <ja_massa97@mail.ru>
2016-09-19 12:48:14 +0300

    delete console logs

--- static/src/app/controllers/editProfileCtrl.js
+++ static/src/app/controllers/editProfileCtrl.js
@@ -40,7 +40,6 @@ pepo.controller('editProfileCtrl', function ($rootScope, $q, $route, $location, 
         $rootScope.success = 1;
       })
       .catch(function(err) {
-        console.log(err);
         $rootScope.success = -1;
         throw new Error('updateUser: editProfileCtrl.js');
       })
