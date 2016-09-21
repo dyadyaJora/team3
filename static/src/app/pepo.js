458f9f942a4fa9fb9c6dbc4e0d42f05a51ca763a
dyadyaJora <ja_massa97@mail.ru>
2016-09-21 18:21:26 +0300

    clear with lint in pepo.js and ymaps.js

--- static/src/app/pepo.js
+++ static/src/app/pepo.js
@@ -1,59 +1,59 @@
 window.pepo = angular.module('pepo', ['ngRoute', 'ngResource', 'satellizer', 'ymaps', 'pepo.config', 'duScroll', 'ngEmoticons', 'angularMoment', 'btford.socket-io'])
-  .config(function($routeProvider, $authProvider, CONFIG) {
-    $routeProvider
-      .when('/', {
-        templateUrl: './build/templates/login.html',
-        controller: 'loginCtrl'
-      })
-      .when('/choose-login', {
-        templateUrl: './build/templates/choose-login.html',
-        controller: 'chooseLoginCtrl'
-      })
-      .when('/@:username', {
-        templateUrl: './build/templates/my-profile.html',
-        controller: 'myProfileCtrl'
-      })
-      .when('/pep:pepId', {
-        templateUrl: './build/templates/single-pep.html',
-        controller: 'singlePepCtrl'
-      })
-        .when('/edit-profile', {
-            templateUrl: './build/templates/edit-profile.html',
-            controller: 'editProfileCtrl'
-        })
-      .when('/feed', {
-          templateUrl: './build/templates/feed.html',
-          controller: 'feedCtrl'
-      })
-      .when('/users', {
-          templateUrl: './build/templates/users.html',
-          controller: 'usersCtrl'
-      })
-      .when('/not-found-:page', {
-          templateUrl: './build/templates/not-found.html',
-          controller: 'notFoundCtrl'
-      })
-      .when('/welcome', {
-          templateUrl: './build/templates/welcome.html',
-          controller: 'welcomeCtrl'
-      })
-      .otherwise({redirectTo: '/'});
+.config(function($routeProvider, $authProvider, CONFIG) {
+  $routeProvider
+  .when('/', {
+    templateUrl: './build/templates/login.html',
+    controller: 'loginCtrl'
+  })
+  .when('/choose-login', {
+    templateUrl: './build/templates/choose-login.html',
+    controller: 'chooseLoginCtrl'
+  })
+  .when('/@:username', {
+    templateUrl: './build/templates/my-profile.html',
+    controller: 'myProfileCtrl'
+  })
+  .when('/pep:pepId', {
+    templateUrl: './build/templates/single-pep.html',
+    controller: 'singlePepCtrl'
+  })
+  .when('/edit-profile', {
+    templateUrl: './build/templates/edit-profile.html',
+    controller: 'editProfileCtrl'
+  })
+  .when('/feed', {
+    templateUrl: './build/templates/feed.html',
+    controller: 'feedCtrl'
+  })
+  .when('/users', {
+    templateUrl: './build/templates/users.html',
+    controller: 'usersCtrl'
+  })
+  .when('/not-found-:page', {
+    templateUrl: './build/templates/not-found.html',
+    controller: 'notFoundCtrl'
+  })
+  .when('/welcome', {
+    templateUrl: './build/templates/welcome.html',
+    controller: 'welcomeCtrl'
+  })
+  .otherwise({redirectTo: '/'});
 
-    $authProvider.facebook({
-      clientId: CONFIG.facebook.clientID,
-      redirectUri: window.location.origin + '/api/auth/facebook',
-      url: '/api/auth/facebook'
-    });
-
-    $authProvider.oauth2({
-      name: 'vkontakte',
-      url: '/api/auth/vkontakte',
-      clientId: CONFIG.vkontakte.clientID,
-      redirectUri: window.location.origin + '/api/auth/vkontakte',
-      authorizationEndpoint: 'https://oauth.vk.com/authorize',
-      optionalUrlParams: ['display'],
-      display: 'mobile'
-    });
-
-    moment.locale('ru');
+  $authProvider.facebook({
+    clientId: CONFIG.facebook.clientID,
+    redirectUri: window.location.origin + '/api/auth/facebook',
+    url: '/api/auth/facebook'
   });
+
+  $authProvider.oauth2({
+    name: 'vkontakte',
+    url: '/api/auth/vkontakte',
+    clientId: CONFIG.vkontakte.clientID,
+    redirectUri: window.location.origin + '/api/auth/vkontakte',
+    authorizationEndpoint: 'https://oauth.vk.com/authorize',
+    optionalUrlParams: ['display'],
+    display: 'mobile'
+  });
+
+  moment.locale('ru');
+});
