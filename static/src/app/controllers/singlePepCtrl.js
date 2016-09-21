0e0d24543022d3d5b6bf2a276ac36400345050e2
dyadyaJora <ja_massa97@mail.ru>
2016-09-21 16:02:41 +0300

    clear with lint singlePepCtrl

--- static/src/app/controllers/singlePepCtrl.js
+++ static/src/app/controllers/singlePepCtrl.js
@@ -1,6 +1,6 @@
-pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, usersApi, MOCKTWEETS) {
+pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, usersApi) {
 
-  currentLocation = [];
+  var currentLocation = [], pepEdit, currentPep;
   navigator.geolocation.getCurrentPosition(show_map);
 
   // Get coordinates.
@@ -13,59 +13,58 @@ pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, u
     $scope.curUser = $scope.currentUser;
   });
 
-  currentPepId = $location.path().slice(4);
+  var currentPepId = $location.path().slice(4);
   pepsApi.getSinglePep({id: currentPepId}).$promise.then(function(data) {
     $scope.currentTweet = data;
     $scope.parent = $scope.currentTweet.parent;
     checkFollow();
-  }).catch(function(err){
+  }).catch(function(){
     $location.path('/not-found-pep');
   });
 
   $scope.isOwner = function(user, tweetOwner) {
-     return user === tweetOwner;
-  }
+    return user === tweetOwner;
+  };
 
   $scope.goToUser = function(username) {
     $location.path('/@' + username);
-  }
+  };
 
   $scope.goToPep = function(pepId) {
     $location.path('/pep' + pepId);
-  }
+  };
 
   function checkFollow() {
-     $scope.$on('currentUserLoaded', function() {
-       $scope.currentUser.following.some(function(followingUser) {
-         if (followingUser === $scope.currentTweet.owner._id) {
+    $scope.$on('currentUserLoaded', function() {
+      $scope.currentUser.following.some(function(followingUser) {
+        if (followingUser === $scope.currentTweet.owner._id) {
           $scope.followed = true;
-         }
-       });
-     });
-   }
+        }
+      });
+    });
+  }
 
   $scope.subscribe = function(username) {
     usersApi.followUser({username: username}).$promise.then(function(){
       $scope.followed = true;
     });
-  }
+  };
 
   $scope.unsubscribe = function(username) {
     usersApi.unfollowUser({username: username}).$promise.then(function(){
       $scope.followed = false;
     });
-  }
+  };
 
 
   $scope.editPepStart = function(index, id, text){
     $scope.editId = id;
-    console.log(id);
     $scope.editPepText = text;
     $scope.editIndex = index;
     $scope.varEdit1 = [];
     $scope.varEdit1[index] = true;
     $scope.emojiOpen = false;
-  }
+  };
 
   $scope.editAnim = [];
   $scope.editMainPep = function(){
@@ -75,29 +74,23 @@ pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, u
       $scope.varEdit1 = [];
       return;
     } 
-    /*
-    if ($scope.tweets[$scope.editIndex].text == $scope.editPepText) {
-      $scope.varEdit1 = [];
-      return;
-    } */
     $scope.editAnim[$scope.editIndex] = true;
     pepEdit = {text: $scope.editPepText };
     pepsApi.editPep({id: $scope.editId}, pepEdit).$promise.then(function(data){
       currentPep = $scope.currentTweet;
       currentPep.text = data.text;
       $scope.varEdit1 = [];
-  }).catch(function(eror){
-    $scope.varEdit1 = [];
-  });
-  setTimeout(function(){ $scope.editAnim = [];}, 2000);
-  }
+    }).catch(function(){
+      $scope.varEdit1 = [];
+    });
+    setTimeout(function(){ $scope.editAnim = [];}, 2000);
+  };
 
   $scope.editPep = function(){
     if($scope.hLinkLenght > $scope.limit || $scope.hLinkLenght == 0) return;
     $scope.emojiOpen = false;
     if ($scope.currentTweet.children[$scope.editIndex].text == $scope.editPepText) {
       $scope.varEdit1 = [];
-      console.log($scope.currentTweet.children[$scope.editIndex].text, $scope.editPepText);
       return;
     } 
     $scope.editAnim[$scope.editIndex] = true;
@@ -110,12 +103,12 @@ pepo.controller('singlePepCtrl', function($location, $scope, pepsApi, userApi, u
       });
       currentPep.text = data.text;
       $scope.varEdit1 = [];
-  }).catch(function(eror){
-    $scope.varEdit1 = [];
-  });
-  setTimeout(function(){ $scope.editAnim = [];}, 2000);
-  }
+    }).catch(function(){
+      $scope.varEdit1 = [];
+    });
+    setTimeout(function(){ $scope.editAnim = [];}, 2000);
+  };
   $scope.addEmojiEdit = function(emoji) {
-     $scope.editPepText += emoji;
-  }
+    $scope.editPepText += emoji;
+  };
 });
